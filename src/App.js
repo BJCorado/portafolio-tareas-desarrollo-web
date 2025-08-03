17e30149387befaed06e7e70a1e71d8fe698215d
BJCorado <brayncorado454@gmail.com>
2025-08-02 21:32:08 -0300

    se modifico el nav menu y se le agrego iconos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaBook, FaCode,} from 'react-icons/fa';
+import { FaBriefcase } from 'react-icons/fa';
 
-import Tareas from './pages/tareas'; // antes era Glossary
+import Tareas from './pages/tareas';
 import Projects from './pages/projects';
 
 function App() {
   return (
     <Router>
+      {/* Menú fijo y moderno */}
+      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+        <Container>
+          <Navbar.Brand className="fw-bold fs-4 text-white d-flex align-items-center gap-2"> <FaBriefcase />Portafolio</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/tareas"><FaBook className="me-2" />Tareas y Recursos</Nav.Link>
+              <Nav.Link as={Link} to="/proyectos"><FaCode className="me-2" />Proyectos</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Contenido de rutas */}
       <div className="container py-4">
         <h1 className="text-center mb-4">Portafolio de Tareas</h1>
-
-        {/* Menú de navegación */}
-        <ul className="nav nav-pills mb-4 justify-content-center">
-          <li className="nav-item">
-            <Link className="nav-link" to="/tareas">Tareas y Recursos</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/proyectos">Proyectos</Link>
-          </li>
-        </ul>
-
-        {/* Rutas de las secciones */}
         <Routes>
           <Route path="/tareas" element={<Tareas />} />
           <Route path="/proyectos" element={<Projects />} />
