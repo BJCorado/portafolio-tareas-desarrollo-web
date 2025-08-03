05bef378fced0fbc2284f93d77a97131b64d222c
BJCorado <brayncorado454@gmail.com>
2025-08-03 16:18:06 -0300

    se mejoraron los nav link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaBook, FaCode,} from 'react-icons/fa';
 import { FaBriefcase } from 'react-icons/fa';
@@ -12,26 +12,31 @@ function App() {
   return (
     <Router>
       {/* Menú fijo y moderno */}
-      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand className="fw-bold fs-4 text-white d-flex align-items-center gap-2"> <FaBriefcase />Portafolio</Navbar.Brand>
+<Navbar className="floating-navbar" expand="lg">
+  <Container>
+    <Navbar.Brand className="fw-bold fs-4 text-white d-flex align-items-center gap-2">
+      <FaBriefcase /> Portafolio De Tareas
+    </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse id="navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/tareas"><FaBook className="me-2" />Tareas y Recursos</Nav.Link>
-              <Nav.Link as={Link} to="/proyectos"><FaCode className="me-2" />Proyectos</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Navbar.Toggle aria-controls="navbar-nav" />
+    <Navbar.Collapse id="navbar-nav">
+     <Nav.Link as={NavLink} to="/tareas">
+  <FaBook className="me-2" /> Tareas y Recursos
+</Nav.Link>
+
+<Nav.Link as={NavLink} to="/proyectos">
+  <FaCode className="me-2" /> Proyectos
+</Nav.Link>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
       {/* Contenido de rutas */}
       <div className="container py-4">
-        <h1 className="text-center mb-4">Portafolio de Tareas</h1>
         <Routes>
           <Route path="/tareas" element={<Tareas />} />
           <Route path="/proyectos" element={<Projects />} />
+          <Route path="/" element={<Navigate to="/tareas" replace />} />
         </Routes>
       </div>
     </Router>
