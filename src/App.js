5ea01789d642cbf8382ec8643d222fba2529e616
BJCorado <brayncorado454@gmail.com>
2025-08-19 19:31:30 -0300

    se dejo de usar route, para mostrar todo en una sola vista

--- src/App.js
+++ src/App.js
@@ -1,55 +1,69 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import ScrollSpy from "bootstrap/js/dist/scrollspy";
+import React, { useEffect } from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaBook, FaCode, FaInfoCircle} from 'react-icons/fa';
-import { FaBriefcase } from 'react-icons/fa';
-
+import { FaBook, FaCode, FaInfoCircle, FaBriefcase } from 'react-icons/fa';
+import { useState } from 'react';
+// Importar componentes de las páginas
 import Tareas from './pages/tareas';
 import Projects from './pages/projects';
 import Acerca from './pages/acerca';
 
-
 function App() {
+  useEffect(() => {
+    // Activa ScrollSpy sobre el body y apunta al contenedor con los <a.nav-link>
+    const spy = new ScrollSpy(document.body, {
+      target: "#navbar-nav",
+      offset: 80,
+    });
+    return () => spy.dispose();
+  }, []);
+
+  // Estado para controlar el colapso
+  const [expanded, setExpanded] = useState(false);
+
   return (
-     <div className="app-wrapper"> {/*CONTENEDOR QUE ENVUELVE TODO */}
-    <Router>
-      {/* Menú fijo y moderno */}
-<Navbar className="floating-navbar" expand="lg" collapseOnSelect>
-  <Container>
-    <Navbar.Brand className="fw-bold fs-4 text-white d-flex align-items-center gap-2">
-      <FaBriefcase /> Portafolio De Tareas
-    </Navbar.Brand>
+    <div className="app-wrapper">
+      <Navbar className="floating-navbar" expand="lg" collapseOnSelect expanded={expanded}
+        onToggle={() => setExpanded(!expanded)}>
+        <Container>
+          <Navbar.Brand className="fw-bold fs-4 text-white d-flex align-items-center gap-2">
+            <FaBriefcase /> Portafolio De Tareas
+          </Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="navbar-nav" />
-    <Navbar.Collapse id="navbar-nav">
-     <Nav.Link as={NavLink} to="/tareas" eventKey="tareas">
-  <FaBook className="me-2" /> Tareas y Recursos
-</Nav.Link>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto" onSelect={() => setExpanded(false)}>
+              <Nav.Link href="#tareas">
+                <FaBook className="me-2" /> Tareas y Recursos
+              </Nav.Link>
+              <Nav.Link href="#proyectos">
+                <FaCode className="me-2" /> Proyectos Web
+              </Nav.Link>
+              <Nav.Link href="#acerca">
+                <FaInfoCircle className="me-2" /> Acerca de
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-<Nav.Link as={NavLink} to="/proyectos" eventKey="proyectos">
-  <FaCode className="me-2" /> Proyectos Web
-</Nav.Link>
-
-<Nav.Link as={NavLink} to="/acerca" eventKey="acerca">
-  <FaInfoCircle className="me-2" /> Acerca de
-</Nav.Link>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-      {/* Contenido de rutas */}
-      <div className="container py-4 main-content">
-        <Routes>
-          <Route path="/tareas" element={<Tareas />} />
-          <Route path="/proyectos" element={<Projects />} />
-          <Route path="/acerca" element={<Acerca />} />
-          <Route path="/" element={<Navigate to="/tareas" replace />} />
-        </Routes>
-      </div>
-    </Router>
+      {/* Contenido (secciones) */}
+      <main className="container py-4 main-content">
+        <section id="tareas" className="mb-5">
+          <Tareas />
+        </section>
+        <section id="proyectos" className="mb-5">
+          <Projects />
+        </section>
+        <section id="acerca" className="mb-5">
+          <Acerca />
+        </section>
+      </main>
     </div>
   );
 }
 
 export default App;
+
